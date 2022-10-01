d65d3da8ea8f8a8337bbd9047a82de55c19f4cdb
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2022-10-01 19:01:04 +0900

    [Silver V] Title: 숫자 카드, Time: 632 ms, Memory: 254396 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -3,13 +3,11 @@ have = list(map(int, input().split()))
 have_dic = {}
 
 for elt in have:
-    have_dic[elt] = 1
+    have_dic[elt] = '1'
 
 M = int(input())
 wonder = list(map(int, input().split()))
 
+
 for elt in wonder:
-    if elt in have_dic.keys():
-        print(1, end = ' ')
-    else:
-        print(0, end = ' ')
+    print(have_dic.get(elt, '0'), end=' ')
