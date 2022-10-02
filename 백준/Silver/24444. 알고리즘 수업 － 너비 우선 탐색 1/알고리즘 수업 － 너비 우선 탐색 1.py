bf5ebdb4ffec874150bcca17c03b106583fbf94a
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2022-10-02 17:18:38 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 604 ms, Memory: 156972 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+def BFS(v):
+    global cnt
+    queue = deque()
+    queue.append(v)
+    while queue:
+        v = queue.popleft()
+        visited[v] = 1
+        cnt += 1
+        dic[v] = cnt
+        for i in graph[v]:
+            if not visited[i]:
+                visited[i] = 1
+                queue.append(i)
+
+N, M, R = map(int, input().split())      # N : 정점의 수 / M : 간선의 수 / R : 시작 정점
+
+visited = [0] * (N+1)
+graph = [[] for _ in range(N+1)]
+cnt = 0
+dic = {}
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for elt in graph:
+    elt.sort()
+
+BFS(R)
+# print(dic)
+for i in range(1, N+1):
+    if i in dic.keys():
+        print(dic[i])
+    else:
+        print(0)
