4eec2fd0bd2c88ba0e309614ded2f2fc6aacb156
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2022-10-03 21:25:05 +0900

    [Silver III] Title: N과 M (1), Time: 184 ms, Memory: 119636 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -5,16 +5,13 @@ def f(i, k, r):
         print(*p)
     else:
         for j in range(k):
-            if not visited[j]:
-                visited[j] = 1
+            if not used[j]:
+                used[j] = 1
                 p[i] = a[j]
                 f(i+1, k, r)
-                visited[j] = 0
+                used[j] = 0
 
 a = [i for i in range(1, N+1)]
-
-visited = [0] * N
-
 p = [0] * M
-
-f(0, N, M)
+used = [0] * len(a)
+f(0, len(a), M)
