39c1d01ce986583c8ca475b5e46602d300507d32
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2022-10-06 10:16:06 +0900

    [Silver III] Title: 터렛, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.py
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -1,20 +1,31 @@
-T = int(input())
-for t in range(T):
-    x1, y1, r1, x2, y2, r2 = map(int,input().split())
-    distance = ((abs(x1-x2)**2)+(abs(y1-y2)**2))**(1/2)
-    if x1 != x2 or y1 != y2:
-        if r1 + r2 < distance or r2 > r1 + distance or r1 > r2 + distance:
-            print(0)
-        elif r1 + r2 == distance or distance == r2 - r1 or distance == r1 - r2:
-            print(1)
-        elif r1 + r2 > distance:
-            print(2)
+import math
 
-    else:
+t = int(input())
+for _ in range(t):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    d = (x1 - x2) ** 2 + (y1 - y2) ** 2
+    if d == 0:
         if r1 == r2:
-            if r1 == 0:
+            if r1 == 0 and r2 == 0:
                 print(1)
-            else:
-                print(-1)
+                continue
+            print(-1)
+            continue
         else:
             print(0)
+            continue
+    elif (r1 + r2) ** 2 > d:
+        if max(r1, r2) == math.sqrt(d) + min(r1, r2):
+            print(1)
+            continue
+        elif max(r1, r2) > math.sqrt(d) + min(r1, r2):
+            print(0)
+            continue
+        print(2)
+        continue
+    elif (r1 + r2) ** 2 == d:
+        print(1)
+        continue
+    else:
+        print(0)
+        continue
