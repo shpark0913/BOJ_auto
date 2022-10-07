b471d97d4cb153e9828192322f9c5e339e4b814b
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2022-10-07 15:25:49 +0900

    [Silver I] Title: 떡 먹는 호랑이, Time: 128 ms, Memory: 114976 KB -BaekjoonHub

---
+++ 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.py
@@ -0,0 +1,23 @@
+D, K = map(int, input().split())
+
+def f():
+    global D
+    global K
+    ricecake = [0] * (D + 1)
+
+    ricecake[D] = K
+    while 1:
+        for i in range(1, K):
+            ricecake[D-1] = K-i
+            for j in range(2, D):
+                ricecake[D-j] = ricecake[D-(j-2)] - ricecake[D-(j-1)]
+                if ricecake[D-j] > ricecake[D-(j-1)]:
+                    continue
+            a = sorted(ricecake)
+            if a == ricecake:
+                print(ricecake[1])
+                print(ricecake[2])
+                return
+
+
+f()
