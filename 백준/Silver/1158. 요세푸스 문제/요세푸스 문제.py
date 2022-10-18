77db77dff6664cf5fc2401e69c5d69462bbbfa75
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2022-10-18 20:11:53 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -4,15 +4,8 @@ arr2 = []
 idx = K - 1
 
 for i in range(N):
-    if len(arr) > idx:
-        arr2.append(arr.pop(idx))
-        idx += (K-1)
-    elif len(arr) <= idx:
-        idx = idx % len(arr)
-        arr2.append(arr.pop(idx))
-        idx += (K-1)
+    idx = idx % len(arr)
+    arr2.append(arr.pop(idx))
+    idx += (K-1)
 
-print('<', end='')
-for i in range(len(arr2)-1):
-    print(f'{arr2[i]}, ', end='')
-print(f'{arr2[-1]}>')
+print(f'<{str(arr2)[1:-1]}>')
