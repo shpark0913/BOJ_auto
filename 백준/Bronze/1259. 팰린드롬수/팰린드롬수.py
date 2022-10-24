795d50d0172aaaf11799e5958f1ec20bd5007393
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2022-10-25 08:22:46 +0900

    [Bronze I] Title: 팰린드롬수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,6 +1,5 @@
 while 1:
-    if (n := str(input())) == '0':
-        break
-    m = list(map(int, n))
-    m_reverse = m[::-1]
-    print('yes') if m == m_reverse else print('no')
+    if (n:=str(input()))=='0':break
+    m=list(map(int, n))
+    p=m[::-1]
+    print('yes') if m==p else print('no')
