3dbf46b9579a3c50ca52f0416fe02aad3ec92602
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2022-11-01 14:40:01 +0900

    [Silver IV] Title: 제로, Time: 192 ms, Memory: 119840 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,6 +1,4 @@
-K = int(input())
-stack = []
-
-for _ in range(K):
-    stack.append(n) if (n := int(input())) else stack.pop()
-print(sum(stack))
+s = []
+for _ in range(K := int(input())):
+    s.append(n) if (n := int(input())) else s.pop()
+print(sum(s))
