948c15792a1357fb36c51d308069ab8b30a504ec
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2022-11-01 09:43:36 +0900

    [Silver IV] Title: 30, Time: 164 ms, Memory: 121572 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.py
+++ 백준/Silver/10610. 30/30.py
@@ -1,11 +1,14 @@
 N = list(map(int, input()))
 
+if sum(N) % 3:
+    print(-1)
+    exit()
+
 N_list = sorted(N, reverse=True)
 
-if N_list[-1] or sum(N_list) % 3:
+if N_list[-1]:
     print(-1)
+
 else:
-    s = ''
     for elt in N_list:
-        s += str(elt)
-    print(s)
+        print(elt, end='')
