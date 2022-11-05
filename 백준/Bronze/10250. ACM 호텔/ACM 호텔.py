28a686f0ecda8adf6b008003ef04e5b98a22d5ef
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2022-11-06 00:11:48 +0900

    [Bronze II] Title: ACM 호텔, Time: 124 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,8 @@
+for i in range(t:=int(input())):
+    h, w, n = map(int, input().split())
+    num = n//h + 1
+    floor = n % h
+    if n % h == 0:  # h의 배수이면,
+        num = n//h
+        floor = h
+    print(f'{floor*100+num}')
