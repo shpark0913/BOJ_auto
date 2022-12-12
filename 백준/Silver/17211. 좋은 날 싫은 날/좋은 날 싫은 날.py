c2a22b3a9082d897adff739bf9ad9a1d31d6a82b
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2022-12-12 13:35:42 +0900

    [Silver V] Title: 좋은 날 싫은 날, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/17211. 좋은 날 싫은 날/좋은 날 싫은 날.py
+++ 백준/Silver/17211. 좋은 날 싫은 날/좋은 날 싫은 날.py
@@ -10,14 +10,5 @@ if N > 1:
 
 bad = 1000*(1-good)
 good *= 1000
-
-if 10*good - 10*int(good) >= 5:
-    good = int(good) + 1
-else:
-    good = int(good)
-if 10*bad - 10*int(bad) >= 5:
-    bad = int(bad) + 1
-else:
-    bad = int(bad)
-print(good)
-print(bad)
+print(int(good))
+print(int(bad))
