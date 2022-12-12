20aa145cd9abb28dc3bc3d1e6a9fc46b67e67e87
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2022-12-12 13:33:28 +0900

    [Silver V] Title: 좋은 날 싫은 날, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/17211. 좋은 날 싫은 날/좋은 날 싫은 날.py
+++ 백준/Silver/17211. 좋은 날 싫은 날/좋은 날 싫은 날.py
@@ -1,29 +1,23 @@
 N, now = map(int, input().split())
 gg, gb, bg, bb = map(float, input().split())
 
-if not now:
-    good = gg
-    bad = gb
-else:
-    good = bg
-    bad = bb
+if not now: good = gg
+else: good = bg
 
 if N > 1:
     for _ in range(N-1):
-        good_new = good*gg + bad*bg
-        bad_new = good*gb + bad*bb
-        good = good_new
-        bad = bad_new
+        good = good*gg + (1-good)*bg
 
+bad = 1000*(1-good)
 good *= 1000
-bad *= 1000
+
 if 10*good - 10*int(good) >= 5:
     good = int(good) + 1
 else:
     good = int(good)
-
 if 10*bad - 10*int(bad) >= 5:
     bad = int(bad) + 1
 else:
     bad = int(bad)
-print(good, bad)
+print(good)
+print(bad)
