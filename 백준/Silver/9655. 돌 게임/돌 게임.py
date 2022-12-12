25231d60cdacabc30a39aad4584906eb02c4708a
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2022-12-12 13:59:59 +0900

    [Silver V] Title: 돌 게임, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.py
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -1,8 +1,1 @@
-N = int(input())
-
-if not (N//3)%2:
-    if N%3 == 1: print("SK")
-    else: print("CY")
-else:
-    if N%3 == 1: print("CY")
-    else: print("SK")
+print("SK" if (n:=int(input())) % 2 else "CY")
