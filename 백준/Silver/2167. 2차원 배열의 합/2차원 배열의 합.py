e9a82f6f2dd77f8de2ea65e9cbddf2f654b5a216
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2022-12-20 10:36:58 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 4008 ms, Memory: 116968 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -0,0 +1,11 @@
+N, M = map(int, input().split())
+
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+for _ in range(K := int(input())):
+    partialSum = 0
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(x1, x2 + 1):
+        for j in range(y1, y2 + 1):
+            partialSum += arr[i-1][j-1]
+    print(partialSum)
