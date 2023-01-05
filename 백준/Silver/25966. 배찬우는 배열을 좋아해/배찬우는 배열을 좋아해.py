a0bcb7a45ce0d2b9db924740c7e4ee915d54f390
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-01-05 09:33:11 +0900

    [Silver V] Title: 배찬우는 배열을 좋아해, Time: 3320 ms, Memory: 221240 KB -BaekjoonHub

---
+++ 백준/Silver/25966. 배찬우는 배열을 좋아해/배찬우는 배열을 좋아해.py
@@ -0,0 +1,16 @@
+N, M, q = map(int, input().split())
+
+arr = []
+
+for _ in range(N):
+    arr.append(list(map(int, input().split())))
+
+for _ in range(q):
+    query = list(map(int, input().split()))
+    if query[0]:
+        arr[query[1]], arr[query[2]] = arr[query[2]], arr[query[1]]
+    else:
+        arr[query[1]][query[2]] = query[-1]
+
+for elt in arr:
+    print(*elt)
