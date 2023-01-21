835c706b410facbf67d9211b956aa9fb639a05f8
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-01-21 23:48:02 +0900

    [Silver V] Title: 학생 인기도 측정, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/25325. 학생 인기도 측정/학생 인기도 측정.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+names = input().split()
+stuLikes = {}
+for name in names:
+    stuLikes[name] = 0
+
+for _ in range(N):
+    stuLike = input().split()
+    for stu in stuLike:
+        stuLikes[stu] += 1
+
+for stu in sorted(stuLikes, key=lambda x:-stuLikes[x]):
+    print(stu, stuLikes[stu])
