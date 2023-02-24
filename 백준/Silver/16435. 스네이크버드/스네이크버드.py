92d48ed2d189b18ded1d2e54018ff83bf26309f0
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-02-24 17:02:31 +0900

    [Silver V] Title: 스네이크버드, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16435. 스네이크버드/스네이크버드.py
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.py
@@ -1,7 +1,10 @@
+# fruitNum : 과일의 개수, snakeLen : 뱀의 처음 길이
 fruitNum, snakeLen = list(map(int, input().split()))
 
+# 과일의 높이를 순서대로 나타낸 list
 fruitsHeigth = sorted(list(map(int, input().split())))
 
+# 과일 높이가 뱀 높이 이하이면 뱀 높이 +1, 그렇지 않다면 for문 종료
 for i in range(len(fruitsHeigth)):
     if snakeLen >= fruitsHeigth[i]:
         snakeLen += 1
