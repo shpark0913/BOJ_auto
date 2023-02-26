bfc8017df5fc381836b60f0f36845f8551333043
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-02-26 21:53:12 +0900

    [Gold V] Title: 행복 유치원, Time: 292 ms, Memory: 66736 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.py
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 childNum, groupNum = map(int, input().split())
 
 childHeights = list(map(int, input().split()))
@@ -15,8 +18,9 @@ differHeights_order = sorted(differHeights, reverse=True)
 # groupNum 개의 조로 나누고 싶다
 makeGroups = []
 
+# 3조로 나누고 싶으면 2 부분을 나눠야 하므로 range 내부에 -1 해줌
 for i in range(groupNum-1):
     makeGroups.append(differHeights_order[i])
 
-
+# 모든 학생의 키 합에서 계차수열의 큰 숫자들을 뺀다
 print(sum(differHeights) - sum(makeGroups))
