87ea89548dc6b2c8c538ceb8b1a031d1b3c8c862
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-03-18 23:16:54 +0900

    [Silver V] Title: 호텔 방 번호, Time: 364 ms, Memory: 116200 KB -BaekjoonHub

---
+++ 백준/Silver/5671. 호텔 방 번호/호텔 방 번호.py
@@ -0,0 +1,16 @@
+while 1:
+    try:
+        roomNumMin, roomNumMax = map(int, input().split())
+        cnt = 0 # 반복되는 숫자가 없는 roomNum의 개수
+        for roomNum in range(roomNumMin, roomNumMax + 1):
+            isRoom = 1
+            if roomNum >= 10:
+                for i in range(len(str(roomNum)) - 1):
+                    for j in range(i+1, len(str(roomNum))):
+                        if str(roomNum)[i] == str(roomNum)[j]:
+                            isRoom -= 1
+            if isRoom == 1:
+                cnt += 1
+        print(cnt)
+    except:
+        exit(0)
