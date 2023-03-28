2b07e4336ee1df793e0271d44afff9bfbe33b368
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-03-28 23:10:36 +0900

    [Silver V] Title: 효정과 새 모니터, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/20949. 효정과 새 모니터/효정과 새 모니터.py
@@ -0,0 +1,16 @@
+# 모니터의 개수
+N = int(input())
+
+# 각 모니터의 가로 픽셀 수와 세로 픽셀 수를 원소로 가지는 리스트
+monitors = []
+
+for i in range(N):
+    monitors.append([i+1, list(map(int, input().split()))])
+
+for monitor in monitors:
+    monitor.append((monitor[1][1]**2 + monitor[1][0]**2)**(1/2)/24)
+
+monitors = sorted(monitors, key=lambda x:-x[-1])
+
+for monitor in monitors:
+    print(monitor[0])
