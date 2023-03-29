0942e2f87aa9695652f39f8327e4fda83991e031
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-03-29 22:19:02 +0900

    [Silver V] Title: 분수찾기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,18 +1,9 @@
-# X번째 분수를 구하여라
-X = int(input())
-
-n = 1
+X=int(input())
+n=1
 while 1:
-    if (n**2 - n + 2)/2 <= X < ((n+1)**2 - (n + 1) + 2)/2:
-        break
+    if (n**2 - n + 2)/2 <= X < ((n+1)**2 - (n + 1) + 2)/2:break
     n += 1
-
-diff = X - int((n**2 - n + 2)/2)
-
-num = str(1 + diff)
-den = str(n - diff)
-
-if n % 2:
-    print(den + "/" + num)
-else:
-    print(num + "/" + den)
+d=X-int((n**2-n+2)/2)
+w=str(1+d)
+p=str(n-d)
+print(p+"/"+w) if n%2 else print(w+"/"+p)
