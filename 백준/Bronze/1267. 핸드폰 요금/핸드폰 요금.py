1bd791aac9a4ad2ccaaf3751f56ebaa174ac7fc0
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-04-07 23:42:09 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+calls = list(map(int, input().split()))
+
+Y, M = 0, 0
+
+for call in calls:
+    Y += (1 + call//30)*10
+    M += (1 + call//60)*15
+
+if Y < M:
+    print("Y", Y)
+elif Y > M:
+    print("M", M)
+else:
+    print("Y", "M", Y)
