cbdc6806a33e5e5e1b200f41b6362f830a9a8211
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-05-06 17:39:05 +0900

    [Silver III] Title: 빈도 정렬, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
@@ -1,11 +1,9 @@
 import sys
 input = sys.stdin.readline
 
-# 메시지의 길이 N, 숫자의 최댓값 C
 N, C = map(int, input().split())
 numsList = list(map(int, input().split()))
 
-# 숫자들의 빈도수를 나타내는 딕셔너리
 numsDic = {}
 
 for num in numsList:
@@ -14,7 +12,6 @@ for num in numsList:
     except:
         numsDic[num] = 1
 
-# numsDic을 .items()를 활용해 list로 변경, 이후 2번째 원소로 정렬
 numsDic = sorted(numsDic.items(), key=lambda x:x[1], reverse=True)
 
 for num in numsDic:
