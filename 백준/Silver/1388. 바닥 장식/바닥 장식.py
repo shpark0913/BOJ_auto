9bea6f11b6b703f8aa143b33c33d70ab23fcb69a
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-05-11 07:31:48 +0900

    [Silver IV] Title: 바닥 장식, Time: 124 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -0,0 +1,41 @@
+# 백준 1388 (실버 4)
+# https://www.acmicpc.net/problem/1388
+# Python3 / 메모리 : KB / 시간 : ms
+
+# 바닥의 세로 크기 : N, 가로 크기 : M
+N, M = map(int, input().split())
+
+floor = [list(input()) for _ in range(N)]
+visited = [[False] * M for _ in range(N)]
+
+# 필요한 나무 판자의 개수
+boardNum = 0
+
+def dfs(floor, visited, i, j):
+    global boardNum
+    # 방문 처리
+    visited[i][j] = True
+    boardNum += 1
+
+    if floor[i][j] == '-':
+        while 1:
+            if j + 1 < M and floor[i][j+1] == '-':
+                visited[i][j+1] = True
+                j += 1
+            else: 
+                break
+    elif floor[i][j] == '|':
+        while 1:
+            if i + 1 < N and floor[i+1][j] == '|':
+                visited[i+1][j] = True
+                i += 1
+            else:
+                break
+
+
+for i in range(N):
+    for j in range(M):
+        if visited[i][j] == False:
+            dfs(floor, visited, i, j)
+
+print(boardNum)
