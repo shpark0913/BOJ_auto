c7b9f632ccb0fd2e7c0680e33897ed0192cadbe0
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-05-13 13:26:03 +0900

    [Silver II] Title: DFS와 BFS, Time: 468 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,37 +1,48 @@
 from collections import deque
 
+# 정점의 개수, 간선의 개수, 탐색 시작 정점 번호
 N, M, V = map(int, input().split())
 
-graph = [[] for _ in range(N+1)]
+visited_DFS = [False] * (N + 1)
+visited_BFS = [False] * (N + 1)
+
+result_DFS = []
+result_BFS = []
+
+graph = [[] for _ in range(N + 1)]
 
 for _ in range(M):
     s, e = map(int, input().split())
     graph[s].append(e)
     graph[e].append(s)
+
 for elt in graph:
     elt.sort()
-visited = [0] * (N+1)
 
-def dfs(v):
-    visited[v] = 1
-    print(v, end = ' ')
-    for i in graph[v]:
+def dfs(graph, visited, V):
+    global result_DFS
+    visited[V] = True
+    result_DFS.append(V)
+    for i in graph[V]:
         if not visited[i]:
-            dfs(i)
+            dfs(graph, visited, i)
 
-def bfs(v):
-    queue = deque()
-    queue.append(v)
-    visited[v] = 1
-    while queue:
-        x = queue.popleft()
-        print(x, end = ' ')
-        for i in graph[x]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = 1
 
-dfs(V)
-print()
-visited = [0] * (N+1)
-bfs(V)
+def bfs(graph, visited, V):
+    global result_BFS
+    Queue = deque([V])
+    visited[V] = True
+    while Queue:
+        q = Queue.popleft()
+        result_BFS.append(q)
+        for elt in graph[q]:
+            if not visited[elt]:
+                visited[elt] = True
+                Queue.append(elt)
+
+
+dfs(graph, visited_DFS, V)
+bfs(graph, visited_BFS, V)
+
+print(*result_DFS)
+print(*result_BFS)
