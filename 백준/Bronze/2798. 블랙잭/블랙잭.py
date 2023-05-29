375b786e669a459d024d89f3a7c79a3143d28587
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-05-29 09:20:37 +0900

    [Bronze II] Title: 블랙잭, Time: 92 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -3,16 +3,12 @@ N, M = map(int, input().split())
 
 cards = sorted(list(map(int, input().split())), reverse=True)
 
-candidates = []
+candidate = 0
 
 for i in range(len(cards) - 2):
     for j in range(i + 1, len(cards) - 1):
         for k in range(j + 1, len(cards)):
-            candidates.append(cards[i] + cards[j] + cards[k])
+            if (cards[i] + cards[j] + cards[k] <= M) and (cards[i] + cards[j] + cards[k] > candidate):
+                candidate = cards[i] + cards[j] + cards[k]
 
-candidates.sort(reverse=True)
-
-for candidate in candidates:
-    if candidate <= M:
-        print(candidate)
-        exit(0)
+print(candidate)
