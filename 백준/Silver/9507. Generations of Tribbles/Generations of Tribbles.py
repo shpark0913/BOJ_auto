2c816ec4d4141aeafaa54d6b35dcae0798d2be41
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-06-18 23:40:14 +0900

    [Silver IV] Title: Generations of Tribbles, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9507. Generations of Tribbles/Generations of Tribbles.py
+++ 백준/Silver/9507. Generations of Tribbles/Generations of Tribbles.py
@@ -1,18 +1,13 @@
 import sys
 input = sys.stdin.readline
-
 dp = [0] * 68
 n = int(input())
-
 dp[0] = 1
 dp[1] = 1
 dp[2] = 2
 dp[3] = 4
-
 for i in range(4, 68):
     dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3] + dp[i - 4]
-
 for _ in range(n):
     num = int(input())
     print(dp[num])
-
