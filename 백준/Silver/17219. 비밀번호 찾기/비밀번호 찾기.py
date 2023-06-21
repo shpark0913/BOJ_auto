3cd1236e6ac46a3d6b1079e4886fa9f3405c1a32
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-06-21 11:14:52 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 8540 ms, Memory: 49344 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,12 +1,15 @@
+import sys
+inputFast = sys.stdin.readline
+
 # 저장된 사이트 주소의 수, 비밀번호 찾으려는 사이트 주소의 수
-N, M = map(int, input().split())
+N, M = map(int, inputFast().split())
 
 informations = {}
 
 for _ in range(N):
-    address, pw = input().split()
+    address, pw = inputFast().split()
     informations[address] = pw
 
 for _ in range(M):
-    isAddress = input()
+    isAddress = input().rstrip()
     print(informations[isAddress])
