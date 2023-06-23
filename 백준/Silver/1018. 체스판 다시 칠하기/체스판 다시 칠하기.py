633d79cc79e68c08eaa601354be2b47cba073326
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-06-23 16:47:54 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 172 ms, Memory: 115696 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,44 @@
+N, M = map(int, input().split())
+
+chess_board = []
+
+for _ in range(N):
+    chess = input()
+    chess_board.append(chess)
+
+min_count = int(1e9)
+WB = 'WBWBWBWB'
+BW = 'BWBWBWBW'
+
+
+def count_change_color(i, j):
+    global min_count
+    cnt_WB = 0
+    cnt_BW = 0
+    for a in range(8):
+        if a % 2:
+            for b in range(8):
+                if chess_board[i + a][j + b] != WB[b]:
+                    cnt_WB += 1
+        else:
+            for b in range(8):
+                if chess_board[i + a][j + b] != BW[b]:
+                    cnt_WB += 1
+    for a in range(8):
+        if not a % 2:
+            for b in range(8):
+                if chess_board[i + a][j + b] != WB[b]:
+                    cnt_BW += 1
+        else:
+            for b in range(8):
+                if chess_board[i + a][j + b] != BW[b]:
+                    cnt_BW += 1
+    if min(cnt_WB, cnt_BW) < min_count:
+        min_count = min(cnt_WB, cnt_BW)
+
+
+for i in range(N - 7):
+    for j in range(M - 7):
+        count_change_color(i, j)
+
+print(min_count)
