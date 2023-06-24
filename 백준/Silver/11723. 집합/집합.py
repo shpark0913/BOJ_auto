3e7dde8e93efd1f98cffca5f58eccf664f828ceb
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-06-24 21:17:11 +0900

    [Silver V] Title: 집합, Time: 4832 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+M = int(input())
+
+S = set()
+for _ in range(M):
+    calculation = list(input().split())
+
+    if calculation[0] == "add":
+        S.add(int(calculation[1]))
+    elif calculation[0] == "remove":
+        if int(calculation[1]) in S:
+            S.discard(int(calculation[1]))
+    elif calculation[0] == "check":
+        if int(calculation[1]) in S:
+            print(1)
+        else:
+            print(0)
+    elif calculation[0] == "toggle":
+        if int(calculation[1]) in S:
+            S.discard(int(calculation[1]))
+        else:
+            S.add(int(calculation[1]))
+    elif calculation[0] == "all":
+        S = {i for i in range(1, 21)}
+    elif calculation[0] == "empty":
+        S = set()
