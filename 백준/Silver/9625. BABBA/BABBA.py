601f01accd3d267f7b15828a05683b8bac12bf68
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-07-02 20:05:36 +0900

    [Silver V] Title: BABBA, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9625. BABBA/BABBA.py
+++ 백준/Silver/9625. BABBA/BABBA.py
@@ -1,11 +1,7 @@
-K = int(input())
-
-dp = [[] for _ in range(K + 1)]
-
-dp[0] = [1, 0]
-
-for i in range(1, K + 1):
-    A, B = dp[i - 1][0], dp[i - 1][1]
-    dp[i] = B, A + B
-
-print(*dp[i])
+K=int(input())
+d=[[] for _ in range(K+1)]
+d[0]=[1, 0]
+for i in range(1,K+1):
+    A,B=d[i-1][0],d[i-1][1]
+    d[i]=B,A+B
+print(*d[i])
