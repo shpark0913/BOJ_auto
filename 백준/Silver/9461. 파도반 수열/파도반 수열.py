31faaccb9423cb8415cbb2bcdf323d10877dd925
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-07-06 00:59:43 +0900

    [Silver III] Title: 파도반 수열, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -2,20 +2,10 @@ T = int(input())
 
 for _ in range(T):
     N = int(input())
-    if N >= 10:
-        dp = [1] * (N + 1)
-    else:
-        dp = [1] * 11
-    dp[4] = 2
-    dp[5] = 2
-    dp[6] = 3
-    dp[7] = 4
-    dp[8] = 5
-    dp[9] = 7
-    dp[10] = 9
+    dp = [1, 1, 1, 1, 2, 2, 3, 4, 5, 7, 9]
     if N <= 10:
         print(dp[N])
     else:
         for i in range(11, N + 1):
-            dp[i] = dp[i - 2] + dp[i - 3]
+            dp.append(dp[i - 2] + dp[i - 3])
         print(dp[N])
