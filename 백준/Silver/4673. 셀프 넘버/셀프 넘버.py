dc48a26d5fb11964c477832d8dfaa1ba43caae18
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-07-10 23:26:15 +0900

    [Silver V] Title: 셀프 넘버, Time: 116 ms, Memory: 31768 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,14 +1,14 @@
 nums = list(range(1, 10001))
 
-not_self = []
+not_self = set()
 
 for num in nums:
     for n in str(num):
         num += int(n)
     if num <= 10000:
-        not_self.append(num)
+        not_self.add(num)
 
-for num in set(not_self):
+for num in not_self:
     nums.remove(num)
 
 for num in nums:
