015041129fb3c82479587b5a7c0f2288738eeda3
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-07-22 23:43:43 +0900

    [Silver V] Title: 회문인 수, Time: 144 ms, Memory: 115376 KB -BaekjoonHub

---
+++ 백준/Silver/11068. 회문인 수/회문인 수.py
@@ -0,0 +1,24 @@
+def ftn(a,b):
+    ans = []
+    while a > 0:
+        ans.append(a % b)
+        a = a//b
+    ans = ans[::-1]
+    return ans
+
+def pal(s):
+    for i in range(len(s) // 2):
+        if s[i] != s[-i - 1]:
+            return 0
+    return 1
+
+t=int(input())
+
+for _ in range(t):
+    cnt = 0
+    n = int(input())
+    for i in range(2, 65):
+        if pal(ftn(n, i)):
+            cnt = pal(ftn(n, i))
+            break
+    print(1) if cnt else print(0)
