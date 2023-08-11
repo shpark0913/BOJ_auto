d1961904716ba4bc77e302fa8beb1b5be1e0d687
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-08-11 23:35:27 +0900

    [Silver IV] Title: 베스트셀러, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+books = {}
+
+for _ in range(N):
+    book = input()
+    if book in books.keys():
+        books[book] += 1
+    else:
+        books[book] = 1
+
+books_list = sorted(books.items(), key=lambda x: (-x[1], x[0]))
+
+print(books_list[0][0])
