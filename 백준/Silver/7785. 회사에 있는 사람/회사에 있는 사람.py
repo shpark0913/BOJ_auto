6b673364303499e37246d626dfcc41881ec44576
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-08-14 23:38:37 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 288 ms, Memory: 163320 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+people = [input().split() for _ in range(N)]
+
+dictionary = {}
+
+for person in people:
+    if person[0] in dictionary.keys():
+        dictionary[person[0]] += 1
+    else:
+        dictionary[person[0]] = 1
+
+ans = []
+
+for a, b in dictionary.items():
+    if b % 2:
+        ans.append(a)
+
+ans.sort(reverse=True)
+
+for a in ans:
+    print(a)
