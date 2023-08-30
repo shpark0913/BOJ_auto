b90b5d32da16e195e6bafefc7c0bfc413cb5242a
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-08-30 22:13:59 +0900

    [Bronze I] Title: 차량 번호판 1, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/16968. 차량 번호판 1/차량 번호판 1.py
+++ 백준/Bronze/16968. 차량 번호판 1/차량 번호판 1.py
@@ -1,7 +1,9 @@
 s = input()
+
 num_c = 26
 num_d = 10
 ans = 1
+
 for i in range(len(s)):
     if s[i] == "c":
         ans_i = 26
@@ -11,4 +13,5 @@ for i in range(len(s)):
         if s[i-1] == s[i]:
             ans_i -= 1
     ans *= ans_i
+
 print(ans)
