c78579b80f157362ed01875a6770b56604ab7f76
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-09-19 23:18:42 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 128 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.py
@@ -0,0 +1,6 @@
+n = int(input())
+for i in range(1, n + 1):
+    if i in [1, n]:
+        print(" " * (n - i) + "*" * (2 * i - 1))
+    else:
+        print(" " * (n - i) + "*" + " " * (2 * (i - 1) - 1) + "*")
