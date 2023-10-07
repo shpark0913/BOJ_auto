fb3a6a2d569224ca673b7ff1697197d9202401f0
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-10-07 21:22:23 +0900

    [Silver III] Title: 바이러스, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,29 +1,31 @@
-from collections import deque
+# 컴퓨터의 수
+computerNum = int(input())
 
-N = int(input())
+# 간선의 개수
+edgeNum = int(input())
 
-E = int(input())
+# 연결 리스트
+adjList = [[] for _ in range(computerNum + 1)]
+visited = [False] * (computerNum + 1)
 
-def bfs(v):
-    global cnt
-    queue = deque()
-    queue.append(v)
-    while queue:
-        v = queue.popleft()
-        visited[v] = 1
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = 1
-                cnt += 1
+# 웜 바이러스에 감염된 컴퓨터의 수
+virusCom = -1
 
-visited = [0] * (N+1)
-graph = [[] for _ in range(N+1)]
-
-for _ in range(E):
+for _ in range(edgeNum):
     s, e = map(int, input().split())
-    graph[s].append(e)
-    graph[e].append(s)
-cnt = 0
-bfs(1)
-print(cnt)
+    adjList[s].append(e)
+    if s not in adjList[e]:
+        adjList[e].append(s)
+
+def dfs(graph, v, visited):
+    global virusCom
+    visited[v] = True
+    virusCom += 1
+    if graph[1] == []:
+        return 0
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+    return virusCom
+
+print(dfs(adjList, 1, visited))
