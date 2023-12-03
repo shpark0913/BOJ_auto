c12de02b333b02092f8a8d07cff62420578786da
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-12-03 22:56:15 +0900

    [level 2] Title: 타겟 넘버, Time: 2.71 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,13 +1,30 @@
-from itertools import product
+# from itertools import product
+
+# def solution(numbers, target):
+#     ans = 0
+#     data = [1, -1]
+#     results = list(product(data, repeat=len(numbers)))
+#     for result in results:
+#         answer = 0
+#         for i in range(len(result)):
+#             answer += (result[i] * numbers[i])
+#         if target == answer:
+#             ans += 1
+#     return ans
+
+answer = 0
+
+def dfs(index, numbers, target, value):
+    global answer
+    if index == len(numbers):
+        if target == value:
+            answer += 1
+            return
+        return
+    dfs(index + 1, numbers, target, value + numbers[index])
+    dfs(index + 1, numbers, target, value - numbers[index])
 
 def solution(numbers, target):
-    ans = 0
-    data = [1, -1]
-    results = list(product(data, repeat=len(numbers)))
-    for result in results:
-        answer = 0
-        for i in range(len(result)):
-            answer += (result[i] * numbers[i])
-        if target == answer:
-            ans += 1
-    return ans
+    global answer
+    dfs(0, numbers, target, 0)
+    return answer
