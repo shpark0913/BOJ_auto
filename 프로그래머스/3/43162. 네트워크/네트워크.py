9e2c69bf2d598ed36639db3aab523bfde7dcb672
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-12-03 14:33:31 +0900

    [level 3] Title: 네트워크, Time: 0.19 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,31 +1,24 @@
 from collections import deque
 
-def bfs(graph, visited, v):
-    queue = deque()
-    visited[v] = True
-    queue.append(v)
+def bfs(graph, visited, v, distance):
+    print("#################")
+    visited[v] = distance
+    queue = deque([v])
     while queue:
         q = queue.popleft()
-        for i in graph[q]:
-            if not visited[i]:
-                visited[i] = True
+        print("q :", q)
+        for i in range(len(graph[q])):
+            if not visited[i] and graph[q][i]:
+                visited[i] = distance
                 queue.append(i)
+        
 
 def solution(n, computers):
-    # n: 컴퓨터의 개수 / computers: 연결에 대한 정보
-    
-    graph = [[] for _ in range(n)]
+    distance = 0
     visited = [False] * n
-    answer = 0
-    
-    for i in range(n):
-        for j in range(n):
-            if i != j and computers[i][j]:
-                graph[i].append(j)
-    
     for i in range(n):
         if not visited[i]:
-            answer += 1
-            bfs(graph, visited, i)
-    
+            distance += 1
+            bfs(computers, visited, i, distance)
+    answer = max(visited)
     return answer
