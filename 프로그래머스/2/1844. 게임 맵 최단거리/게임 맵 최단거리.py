30d3d91d748684231cb24ccb458709a7e84b0bea
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-12-05 22:50:11 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.89 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,22 +1,25 @@
 from collections import deque
 
-def bfs(graph, x, y, visited):
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, -1, 1]  
-    visited[x][y] = 1
-    queue = deque([[x, y]])
-    while queue:
-        [x1, y1] = queue.popleft()
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(graph, visited, a, b):
+    q = deque([(a, b)])
+    visited[a][b] = 1
+    while q:
+        x, y = q.popleft()
         for i in range(4):
-            nx = x1 + dx[i]
-            ny = y1 + dy[i]
+            nx, ny = x + dx[i], y + dy[i]
             if 0 <= nx < len(graph) and 0 <= ny < len(graph[0]) and not visited[nx][ny] and graph[nx][ny] == 1:
-                visited[nx][ny] = visited[x1][y1] + 1
-                queue.append([nx, ny])
+                visited[nx][ny] = visited[x][y] + 1
+                q.append((nx, ny))
 
 def solution(maps):
     visited = [[False] * len(maps[0]) for _ in range(len(maps))]
-    bfs(maps, 0, 0, visited)
-    answer = visited[-1][-1]
-    if not answer: return -1
-    return answer
+    bfs(maps, visited, 0, 0)
+    
+    answer = visited[len(maps) - 1][len(maps[0]) - 1]
+    if answer:
+        return answer
+    else:
+        return -1
