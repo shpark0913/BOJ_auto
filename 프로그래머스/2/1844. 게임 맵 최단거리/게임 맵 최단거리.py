d344e6e39f3b9031f4284004be5fb5e07ce0dfbf
shpark0913 <109323665+shpark0913@users.noreply.github.com>
2023-12-08 10:44:11 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.26 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,30 +1,27 @@
-import heapq
+from collections import deque
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def dijkstra(a, b, maps, distance):
-    q = []
-    distance[a][b] = 1
-    heapq.heappush(q, (1, (a, b)))
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now[0]][now[1]] < dist:
-            continue
+def bfs(maps, visited, a, b, N, M):
+    queue = deque([(a, b)])
+    visited[a][b] = True
+    while queue:
+        x, y = queue.popleft()
         for i in range(4):
-            nx, ny = now[0] + dx[i], now[1] + dy[i]
-            if 0 <= nx < len(maps) and 0 <= ny < len(maps[0]) and maps[nx][ny] == 1:
-                cost = dist + 1
-                if cost < distance[nx][ny]:
-                    distance[nx][ny] = cost
-                    heapq.heappush(q, (cost, (nx, ny)))
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == False and maps[nx][ny] == 1:
+                maps[nx][ny] = maps[x][y] + 1
+                visited[nx][ny] = True
+                queue.append((nx, ny))
 
 def solution(maps):
-    INF = int(1e9)
-    distance = [[INF] * len(maps[0]) for _ in range(len(maps))]
-    dijkstra(0, 0, maps, distance)
-    answer = distance[-1][-1]
-    if answer != INF:
-        return answer
+    N, M = len(maps), len(maps[0])
+    
+    visited = [[False] * M for _ in range(N)]
+    bfs(maps, visited, 0, 0, N, M)
+
+    if visited[-1][-1]:
+        return maps[-1][-1]
     else:
         return -1
