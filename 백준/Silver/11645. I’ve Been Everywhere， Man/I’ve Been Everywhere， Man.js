e1d4d7188b0c7feb9fc08c0700f5e62d53480fde
Sihyun Park <109323665+shpark0913@users.noreply.github.com>
2025-04-20 00:14:13 +0900

    [Silver V] Title: I’ve Been Everywhere, Man, Time: 96 ms, Memory: 9868 KB -BaekjoonHub

--- 백준/Silver/11645. I’ve Been Everywhere， Man/I’ve Been Everywhere， Man.js
+++ 백준/Silver/11645. I’ve Been Everywhere， Man/I’ve Been Everywhere， Man.js
@@ -2,18 +2,12 @@ const [T, ...inputs] = require("fs")
   .readFileSync(0, "utf-8").trim()
   .split("\n");
 
-let visited = new Set()
 for (let i = 0; i < inputs.length; i++) {
-  // 숫자인 경우
   if (!isNaN(Number(inputs[i]))) {
-    if (i === 0) continue
-    console.log(visited.size)
-    visited.clear()
-  } else {
-    visited.add(inputs[i])
-  }
-
-  if (i === inputs.length - 1) {
+    let visited = new Set()
+    for (let j = i + 1; j < Number(inputs[i]) + i + 1; j++) {
+      visited.add(inputs[j])
+    }
     console.log(visited.size)
   }
 }
